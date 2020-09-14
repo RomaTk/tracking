2db6ebcdd46685fdf6d31bec34cd7221699fae4b
RomaTk <rt.craz@gmail.com>
2020-09-14 19:40:13 +0300

    beautify

--- scripts/define-object-type/index.js
+++ scripts/define-object-type/index.js
@@ -1,18 +1,24 @@
+/**
+ * 
+ * Function returns 'A' or 'B' or 'C' or 'D' 
+ * If it changed it won't say that is was changed (type A)
+ * if it changed it will say that it was changed (type B)
+ * if its property was changed it will say that it was changed (type C)
+ * if it is object created by this library (type D)
+ * 
+ * @param object {*} The type of this object should be defined 
+ * 
+ */
+
 export default (object) => {
+	let name = typeof object;
 
-    var name = typeof object;
-
-    if (name === 'number') {
-        return 'A';
-    }
-    else if (name === 'string') {
-        return 'B';
-    }
-    else {
-        return 'C';
-    }
+	//TODO implement type D
+	if (name === 'number') {
+		return 'A';
+	} else if (name === 'string') {
+		return 'B';
+	} else {
+		return 'C';
+	}
 };
-
-//числа - тип А  alert(typeof object);
-//строчки - тип В
-//все остальное - тип С
