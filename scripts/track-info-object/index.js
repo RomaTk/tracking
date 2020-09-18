e12c941695521d4c0849f74cba880a87676b363d
RomaTk <rt.craz@gmail.com>
2020-09-19 01:22:45 +0300

    ended

--- scripts/track-info-object/index.js
+++ scripts/track-info-object/index.js
@@ -1,20 +1,28 @@
 import defineObjectType from '../define-object-type/index.js';
+import LabelClass from '../label-class.js';
 
-export default class TrackInfoObject {
+
+/**
+ * This object saves info about some object
+ */
+export default class TrackInfoObject extends LabelClass {
 	/**
 	 * 
-	 * @param {*} object 
-	 * @param func {function | undefined}
+	 * @param {*} object object about which should be info saved
+	 * @param {function | undefined} func  function which should be called when this object changed
 	 * 
 	 */
 	constructor(object = undefined, func = undefined) {
+		super();
 		this.object = object;
 		this.callFunction = func;
 	}
 
+
+
 	/**
 	 *
-	 * @param {*} object
+	 * @param {*} object object about which should be info saved
 	 * 
 	 */
 	set object(object) {
@@ -23,13 +31,21 @@ export default class TrackInfoObject {
 		} else {
 			this._object = object;
 			this._objectType = defineObjectType(object);
-		}
-		
+		}	
 	}
 
 	/**
+	 * @return {*} object about which was info saved
+	 */
+	get object() {
+		return this._object;
+	}
+
+
+
+	/**
 	 *
-	 * @param func {function | undefined}
+	 * @param {function | undefined} func function which should be called when this object changed
 	 * 
 	 */
 	set callFunction(func) {
@@ -38,5 +54,31 @@ export default class TrackInfoObject {
 		} else {
 			//TODO throw error id = 3
 		}
+	}
+
+	/**
+	 * 
+	 * @return {function | undefined} function which calls when this object changes
+	 * 
+	 */
+	get callFunction() {
+		return this._callFunction;
+	}
+
+	/**
+	 * 
+	 * @param {defineObjectType()} objectType type of object about which info was saved
+	 * 
+	 */
+	set objectType(objectType) {
+		//TODO throw error with id = 4
+	}
+	/**
+	 *
+	 * @return {defineObjectType()} type of object about which info was saved
+	 *
+	 */
+	get objectType() {
+		return this._objectType;
 	}
 }
