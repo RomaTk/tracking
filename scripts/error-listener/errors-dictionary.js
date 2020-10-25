335801e3060984327187060ff9ace11d0fd3c22f
RomaTk <rt.craz@gmail.com>
2020-10-25 15:27:08 +0200

    error-dictionary update

--- scripts/error-listener/errors-dictionary.js
+++ scripts/error-listener/errors-dictionary.js
@@ -1,11 +1,9 @@
 /**
  * Object which saves explanation of errors via key
  */
-//TODO remove unnecessary
 export default {
 	0: 'Incorrect parameter type',
 	1: 'There is no error with that id',
 	2: 'The parent has already exists',
-	3: 'There can not be this class of object',
-	4: 'The object should be made by TrackingProxy'
+	4: 'The object should be TrackingProxy'
 }
